8379f1dcdecec3426055c2834502883895cb600b
Cope Chan <1728572473@qq.com>
2022-09-17 19:01:58 +0800

    初步完成登录、注册功能

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,19 @@ var cors = require('cors');
 
 const app = express();
 
-const pool =require('./db');
+const login=require('./routes/login')
+const signup = require('./routes/signup')
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/images', express.static('imgs'));
+
 const routes = require('./routes')(app);
+
+//挂载
+app.use("/login", login);
+app.use("/signup", signup);
 
 const server = app.listen(3333, () => {
   console.log('Listening on port %s...', server.address().port);
