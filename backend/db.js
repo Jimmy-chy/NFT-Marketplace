2bea9638a9bf85372e386039ec0a3c73c1c91816
Cope Chan <1728572473@qq.com>
2022-09-17 19:00:20 +0800

    modify user_tb and admin_tb

--- backend/db.js
+++ backend/db.js
@@ -4,35 +4,55 @@ const config = {
     host: 'localhost',   // 服务器的地址
     port: 3306,          // 端口号
     user: 'root',        // 用户名
-    password: '123456',
-    database: 'ntf',
+    password: '12345678',
+    database: 'nft',
     multipleStatements: true
 }
-
 var pool = mysql.createPool(config);
 
 //for init
 pool.getConnection((err, conn) => {
-    if (!err)
+    if (!err) {
+        console.debug("connect");
+        conn.release();
         return;
-    console.debug("init");
-    if (err.code == 'ER_BAD_DB_ERROR') {
+    }
         
+    console.debug("init");
+
+    if (err.code == 'ER_BAD_DB_ERROR') {
+
         delete config.database;
         conn = mysql.createConnection(config);
-        // //建数据库
-        conn.query(`create database if not exists ntf;`, (err, result) => {
+        //建数据库
+        conn.query(`create database if not exists nft;`, (err, result) => {
             if (err) {
                 console.debug("create db failed");
                 throw err;
             }
         });
 
-        conn.query(`use ntf;`);
+        conn.query(`use nft;`);
 
-        // //建表 @账号：整型；密码、姓名、性别：字符串
-        let sql = `create table if not exists user  ( 账号 int(10), 密码 char(10), 姓名 char(10), 性别 char(1) );` +
-                  `create table if not exists admin ( 账号 int(10), 密码 char(10), 姓名 char(10), 性别 char(1) );`;
+        //建表 @ID：整型；用户名、密码、MetaMask账户地址、性别、邮箱、电话号码、学院、专业
+        // ALTER:id设置为自增,gender 修改为VARCHAR@Chan
+        let sql = `create table if not exists user  ( 
+                    id INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
+                    uname VARCHAR(20) NOT NULL UNIQUE,
+                    pwd VARCHAR(32) NOT NULL,
+                    address VARCHAR(50) NOT NULL UNIQUE,
+                    email VARCHAR(50) DEFAULT NULL,
+                    gender VARCHAR(6),
+                    phone VARCHAR(11),
+                    college VARCHAR(10),
+                    major VARCHAR(10),
+                    PRIMARY KEY(id)
+                );` +
+            `create table if not exists admin (
+                uname VARCHAR(20) NOT NULL UNIQUE,
+                address VARCHAR(50) NOT NULL UNIQUE,
+                pwd VARCHAR(32) NOT NULL
+             );`;
         conn.query(sql, (err, result) => {
             if (err) {
                 console.debug("create table failed");
